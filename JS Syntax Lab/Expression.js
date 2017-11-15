68a70eb67d3989873764f3763cd0fa40dac6f781
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-11-15 13:10:27 +0200

    Adding the node installation configuration and the second problem solution

---
+++ JS Syntax Lab/Expression.js
@@ -0,0 +1,8 @@
+function solution() {
+    let innerExpression = (30 + 25) * 1/3 * (35 - 14 -12);
+
+//let expression = Math.pow(innerExpression, 2);
+    let expression = innerExpression ** 2;
+
+    console.log(expression);
+}
